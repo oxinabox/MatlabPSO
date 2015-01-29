9d0ea021c083aae08560e540742f1eb741eecd3c
oxinabox <oxinabox@ucc.asn.au>
2015-01-29 10:24:08 +0800

    =Removed unneeded files

--- filterStrings.m
+++
@@ -1,9 +0,0 @@
-function [ filtered ] = filterStrings( strings, removeSubstring )
-%FILTERSTRINGS returns all the string input except thos containing the
-%removeSubstring
-    keep = cellfun(@isempty, strfind(strings, removeSubstring));
-    filtered = strings(keep);
-
-
-end
-
